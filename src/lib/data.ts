efe1caf1f04bdddf4b0e947bfb4a864fa2dd9c5d
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2024-09-24 20:10:12 +0200

    add more member

---
+++ src/lib/data.ts
@@ -0,0 +1,22 @@
+export type Member = {
+  name: string;
+  position?: string;
+};
+
+export const members: Member[] = [
+  { name: "Mari G", position: "Leder" },
+  { name: "Ola", position: "Nestleder" },
+  { name: "Sara", position: "Tidligere leder" },
+  { name: "Aurora" },
+  { name: "Torunn", position: "Tillitsvalgt" },
+  { name: "Sasha" },
+  { name: "Gurveer" },
+  { name: "Jonas" },
+  { name: "Ludvig" },
+  { name: "Holger" },
+  { name: "Tuva" },
+  { name: "Ailin" },
+  { name: "Mari Jin" },
+  { name: "Odin" },
+  { name: "Pelle" },
+];
